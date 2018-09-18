16932a46a64bd34c28574e4b8697b904b2e5ef3f
Erin McMackin <mcmackinerin@gmail.com>
2018-09-18 11:20:04 -0400

    Project 1, Part 1 complete; form created

---
+++ CalculateLetterGrade/CalculateLetterGrade/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateLetterGrade
+{
+    public partial class frmCalculateGrade : Form
+    {
+        public frmCalculateGrade()
+        {
+            InitializeComponent();
+        }
+
+        private void txtLetterGrade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
